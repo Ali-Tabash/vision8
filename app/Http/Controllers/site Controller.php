baa53a9821428d3039dd5bac26eb686a1879f1e6
Ali-Tabash <mta20mta@gmail.com>
2022-09-20 01:24:28 +0300

    test

---
+++ app/Http/Controllers/site Controller.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\controllers;
+
+class siteController
+{
+    function index(){
+        return 'Hello World';
+} 
+}
+
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+ 
 use Illuminate\Support\Facades\Route;
+use App\Http\controllers\siteController;
+//use
+//namespace
 
 //Route::get('url','action');
 //Route::post('url','action');
@@ -29,17 +33,17 @@ use Illuminate\Support\Facades\Route;
 //     return 'ali aa';
 // });
 
-Route::get('user/ali',function(){
-       return 'welcome ali';
-});
+// Route::get('user/ali',function(){
+//        return 'welcome ali';
+// });
 
 //  لعمل قيمة متغيرة في الرابط
-Route::get('user/{name}',function($name){
-    return 'welcome' . $name;
-});
-Route::get('/ali/{name}/{age}',function($name,$age){
-    return ' welcome '. $name . 'is age your' .$age;
-})->whereAlpha('name')->whereNumber('age');
+// Route::get('user/{name}',function($name){
+//     return 'welcome' . $name;
+// });
+// Route::get('/ali/{name}/{age}',function($name,$age){
+//     return ' welcome '. $name . 'is age your' .$age;
+// })->whereAlpha('name')->whereNumber('age');
 
 // Route::get('news/{id}',function($id){
 //     return 'News' .$id;
@@ -48,14 +52,14 @@ Route::get('/ali/{name}/{age}',function($name,$age){
 //     return'news';
 // });
 //  (?)لجعل الرابطين يعملوا برابط واحد نستعنى عن الاول ونضع فقط علامة 
-Route::get('news/{id?}',function($id=null){
-    return 'News' .$id;
-});
-// هذه تسمى Optional Parameters
+// Route::get('news/{id?}',function($id=null){
+//     return 'News' .$id;
+// });
+// // هذه تسمى Optional Parameters
 
-Route::get('/user/{name}/{age}',function($name,$age){
-    return ' welcome '. $name . 'is age your' .$age;
-});
+// Route::get('/user/{name}/{age}',function($name,$age){
+//     return ' welcome '. $name . 'is age your' .$age;
+// });
 
 // Route::get('admin/users', function(){  return 'Admin users';  }); 
 // Route::get('admin/posts', function(){ return 'Admin posts';});
@@ -74,16 +78,21 @@ Route::get('/user/{name}/{age}',function($name,$age){
     //     Route::get('payments', function(){ return 'Admin payments';})->name('admin.payments');
     // });
 
-    Route::prefix('admin')->name('admin.')->group(function(){
-        Route::get('users', function(){  return 'Admin users'; })
-        ->name('users'); 
-        Route::get('posts', function(){ return 'Admin posts';})->name('posts');
-        Route::get('comments', function(){ return 'Admin comments';})->name('comment');
-        Route::get('products', function(){ return 'Admin products';})->name('products');
-        Route::get('orders', function(){ return 'Admin orders';})->name('orders');
-        Route::get('payments', function(){ return 'Admin payments';})->name('payments');
-    });
+    // Route::prefix('admin')->name('admin.')->group(function(){
+    //     Route::get('users', function(){  return 'Admin users'; })
+    //     ->name('users'); 
+    //     Route::get('posts', function(){ return 'Admin posts';})->name('posts');
+    //     Route::get('comments', function(){ return 'Admin comments';})->name('comment');
+    //     Route::get('products', function(){ return 'Admin products';})->name('products');
+    //     Route::get('orders', function(){ return 'Admin orders';})->name('orders');
+    //     Route::get('payments', function(){ return 'Admin payments';})->name('payments');
+    // });
 
 
-
+    //  Route::get('/', function () {
+    //         return 'Hello World';
+    //     });
+// الشكل النهائي لرابط 
+     Route::get('/', [siteController::class,'index'])->name('holle world');  
+        
 
